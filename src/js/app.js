63c7234d4b7ff4f4f4462bd162782da3e164bfd6
MayThuPikeSoe <maythupikesoe@gmail.com>
2024-09-13 16:48:08 +0630

    add

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,19 @@
 const products = [
   {
     id: 1,
-    image: "/src/images/man-fg01.webp",
+    image: "/assets/man-fg01.webp",
   },
   {
     id: 2,
-    image: "/src/images/man-fg02.webp",
+    image: "/assets/man-fg02.webp",
   },
   {
     id: 3,
-    image: "/src/images/man-fg03.webp",
+    image: "/assets/man-fg03.webp",
   },
   {
     id: 4,
-    image: "/src/images/man-fg04.webp",
+    image: "/assets/man-fg04.webp",
   },
 ];
 
